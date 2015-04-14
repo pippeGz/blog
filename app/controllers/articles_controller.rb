6f50ea55a25b8b57f63b5a518fc16d66b7cddc7a
Andrés Gz <pippe.111007@gmail.com>
2015-04-14 17:01:06 -0500

    subiendo al repo

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+	before_action :authenticate_user!, except: [:show,:index]
+	before_action :set_article, except: [:index,:new,:create]
+
+	#GET /articles
+	def index
+		# Todos los registros SELECT * FROM 
+		@articles = Article.all
+	end	
+	#GET /articles/:id
+	def show
+		@article.update_visits_count
+		@comment = Comment.new
+	end
+	#GET /articles/new
+	def new
+		@article = Article.new
+	end
+
+	def edit
+	end
+
+	#POST /articles
+	def create
+		@article = current_user.articles.new(article_params)
+
+		if @article.save
+			redirect_to @article
+		else
+			render :new
+		end
+	end
+	#DELETE /artiles/:id
+	def destroy
+		@article.destroy #elimina el objeto de la BD
+		redirect_to articles_path	
+	end
+	#PUT /articles/:id
+	def update
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def set_article
+		@article = Article.find(params[:id])		
+	end
+
+	def article_params
+		params.require(:article).permit(:title,:body)
+	end
+end
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,63 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:update, :destroy]
+  before_action :set_article
+  before_action :authenticate_user!
+
+  respond_to :html
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    @comment.article = @article
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment.article, notice: 'Comment was successfully created'}
+        format.json { render :show, status: :created, location: @comment.article }
+      else
+        format.html { render :new}
+        format.json { render json: @comment.errors, status: :unprocessable_entity }  
+      end  
+    end
+
+    # @comment.save
+    # respond_with(@comment)
+  end
+
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @comment.article, notice: 'Comment was successfully updated'}
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit}
+        format.json { render json: @comment.errors, status: :unprocessable_entity }  
+      end  
+    end
+
+    # @comment.update(comment_params)
+    # respond_with(@comment)
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+        format.html { redirect_to @article, notice: 'Comment was successfully destroyed'}
+        format.json { head :no_content }
+    end      
+
+    # @comment.destroy
+    # respond_with(@comment)
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:article_id])
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:user_id, :article_id, :body)
+    end
+end
